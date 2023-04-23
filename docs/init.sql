28ecc7eb119dd9ff4b7d8cd5e0a2491903f214de
Pro911 <pro911@qq.com>
2023-04-23 15:18:53 +0800

    处理一个文档

--- docs/init.sql
+++ docs/init.sql
@@ -1,6 +1,6 @@
-INSERT INTO "user" ("user_id", "username", "password", "created_at", "updated_at") VALUES (1, 'pro911@qq.com', '123456', '2023-04-19 22:21:27.805725+08:00', '2023-04-19 22:21:27.805725+08:00');
-INSERT INTO "user" ("user_id", "username", "password", "created_at", "updated_at") VALUES (2, 'pro912@qq.com', '123456', '2023-04-19 22:21:33.8632039+08:00', '2023-04-19 22:21:33.8632039+08:00');
-INSERT INTO "user" ("user_id", "username", "password", "created_at", "updated_at") VALUES (3, 'pro913@qq.com', '123456', '2023-04-19 22:21:37.2176086+08:00', '2023-04-19 22:21:37.2176086+08:00');
+INSERT INTO "user" ("user_id", "username", "password", "created_at", "updated_at") VALUES (1, 'proxx01@qq.com', '123456', '2023-04-19 22:21:27.805725+08:00', '2023-04-19 22:21:27.805725+08:00');
+INSERT INTO "user" ("user_id", "username", "password", "created_at", "updated_at") VALUES (2, 'proxx02@qq.com', '123456', '2023-04-19 22:21:33.8632039+08:00', '2023-04-19 22:21:33.8632039+08:00');
+INSERT INTO "user" ("user_id", "username", "password", "created_at", "updated_at") VALUES (3, 'proxx03@qq.com', '123456', '2023-04-19 22:21:37.2176086+08:00', '2023-04-19 22:21:37.2176086+08:00');
 
 
 INSERT INTO "coupon" ("coupon_id", "name", "satisfy", "minus", "desc", "created_at", "updated_at") VALUES (1, '满5元减1元', 500, 100, '满5元减1元,限时大放送！', '2023-04-19 22:21:27.805725+08:00', '2023-04-19 22:21:27.805725+08:00');
