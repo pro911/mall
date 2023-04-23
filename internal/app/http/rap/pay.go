1ad2420d950ae0410a247e011d8774f6c92e31a8
Pro911 <pro911@qq.com>
2023-04-23 16:45:07 +0800

    1

--- internal/app/http/rap/pay.go
+++ internal/app/http/rap/pay.go
@@ -2,5 +2,5 @@ package rap
 
 type PayReq struct {
 	OrderID int    `json:"order_id" binding:"required"`
-	PayType string `json:"pay_type" binding:"required,one of=wechat alipay"`
+	PayType string `json:"pay_type" binding:"required,oneof=wechat alipay"`
 }
