513521cd65eb707cca99edb9d040ff2d1f30da10
Pro911 <pro911@qq.com>
2023-04-23 15:16:57 +0800

    处理sql

---
+++ docs/init.sql
@@ -0,0 +1,10 @@
+INSERT INTO "user" ("user_id", "username", "password", "created_at", "updated_at") VALUES (1, 'pro911@qq.com', '123456', '2023-04-19 22:21:27.805725+08:00', '2023-04-19 22:21:27.805725+08:00');
+INSERT INTO "user" ("user_id", "username", "password", "created_at", "updated_at") VALUES (2, 'pro912@qq.com', '123456', '2023-04-19 22:21:33.8632039+08:00', '2023-04-19 22:21:33.8632039+08:00');
+INSERT INTO "user" ("user_id", "username", "password", "created_at", "updated_at") VALUES (3, 'pro913@qq.com', '123456', '2023-04-19 22:21:37.2176086+08:00', '2023-04-19 22:21:37.2176086+08:00');
+
+
+INSERT INTO "coupon" ("coupon_id", "name", "satisfy", "minus", "desc", "created_at", "updated_at") VALUES (1, '满5元减1元', 500, 100, '满5元减1元,限时大放送！', '2023-04-19 22:21:27.805725+08:00', '2023-04-19 22:21:27.805725+08:00');
+INSERT INTO "coupon" ("coupon_id", "name", "satisfy", "minus", "desc", "created_at", "updated_at") VALUES (2, '满100元减10元', 10000, 1000, '满100元减10元', '2023-04-19 22:21:27.805725+08:00', '2023-04-19 22:21:27.805725+08:00');
+
+INSERT INTO "comment" ("comment_id", "goods_id", "user_id", "desc", "created_at", "updated_at") VALUES (1, 1, 1, 'hhkhjkljkljklj', '2023-04-19 22:21:27.805725+08:00', '2023-04-19 22:21:27.805725+08:00');
+INSERT INTO "comment" ("comment_id", "goods_id", "user_id", "desc", "created_at", "updated_at") VALUES (2, 2, 1, 'sadsadsadsa', '2023-04-19 22:21:27.805725+08:00', '2023-04-19 22:21:27.805725+08:00');
